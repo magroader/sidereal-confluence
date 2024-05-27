b2c65340a8bd08693cd33bec9b811a5e8227a4ba
Colin C <magroader@gmail.com>
2024-05-27 14:35:28 -0400

    Creates default player to begin with rather than rendering some random one

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// Render the initial form
-router.get('/', (req, res) => {
-  res.render('index', { players: [] });
-});
-
-// Handle form submission and score calculation
-router.post('/calculate-scores', (req, res) => {
-  const players = req.body.players.map(player => {
+function createPlayerObj(player, includeScore) {
     const victoryPoints = parseFloat(player.victoryPoints) || 0;
     const ultratech = parseFloat(player.ultratech) || 0;
     const largeCubes = parseFloat(player.largeCubes) || 0;
@@ -16,12 +9,15 @@ router.post('/calculate-scores', (req, res) => {
     const ships = parseFloat(player.ships) || 0;
     const regret = parseFloat(player.regret) || 0;
 
-    const score = victoryPoints 
-      + ultratech * (6 / 12) 
-      + largeCubes * (3 / 12) 
-      + smallCubes * (2 / 12) 
-      + ships * (2 / 12) 
-      + regret * (-1);
+    var score = undefined;
+    if (includeScore) {
+        score = victoryPoints 
+          + ultratech * (6 / 12) 
+          + largeCubes * (3 / 12) 
+          + smallCubes * (2 / 12) 
+          + ships * (2 / 12) 
+          + regret * (-1);
+    }
 
     return {
       name: player.name,
@@ -33,6 +29,17 @@ router.post('/calculate-scores', (req, res) => {
       regret,
       score
     };
+}
+
+// Render the initial form
+router.get('/', (req, res) => {
+  res.render('index', { players: [createPlayerObj({}, false)] });
+});
+
+// Handle form submission and score calculation
+router.post('/calculate-scores', (req, res) => {
+  const players = req.body.players.map(player => {
+    return createPlayerObj(player, true);
   });
 
   players.sort((a, b) => b.score - a.score);
