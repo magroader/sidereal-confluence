4cd1361b539e3db9df8dab0331e85da44a8282c5
Colin C <magroader@gmail.com>
2024-05-27 22:23:51 -0400

    Removing unnecessary dependencies

--- server.js
+++ server.js
@@ -4,16 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Set EJS as the templating engine
-app.set('view engine', 'ejs');
-
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Define the main route
-app.get('/', (req, res) => {
-    res.render('index');
-});
 
 // Start the server
 app.listen(PORT, () => {
